fe095d20618c67784305516f9b349031ac22fc18
bruce <tkntobfrk@gmail.com>
2017-05-19 10:00:34 -0500

    update

--- app.py
+++ app.py
@@ -15,14 +15,16 @@ import pytesseract
 import requests
 from PIL import Image
 
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+
 
 print('Loading tesseract-lambda')
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 LIB_DIR = os.path.join(SCRIPT_DIR, 'lib')
 PSM_FLAG = '-l eng -psm 7'
+DATA_DIR='--tessdata-dir /var/task/tessdata'
+INPUT='/var/task/test.png'
+OUTPUT='/tmp/out.txt'
 
 tmp_dir = tempfile.mkdtemp()
 tmp_file_name = str(uuid.uuid4()) + '.txt'
@@ -43,10 +45,11 @@ def key_name(key):
     return key.split('/')[-1]
 
 def tesseract(image_file):
-    command = 'LD_LIBRARY_PATH={} TESSDATA_PREFIX={} {}/tesseract {} {} {}'.format(
+    command = 'LD_LIBRARY_PATH={} TESSDATA_PREFIX={} {}/tesseract {} {} {} {}'.format(
         LIB_DIR,
         os.path.join(SCRIPT_DIR, 'tessdata'),
         SCRIPT_DIR2,
+        DATA_DIR,
         image_file,
         tmp_result_path,
         PSM_FLAG,
@@ -55,22 +58,39 @@ def tesseract(image_file):
     print('Tesseract command: ' + command)
 
     try:
-        log.debug(command)
         output = subprocess.check_output(command, shell=True)
+        print("OUTPUT GOOD 1")
+        print(output)
+        print("OUTPUT GOOD 2")
         return tmp_result_path
     except subprocess.CalledProcessError as e:
         print(e.output)
+        print("OUTPUT BAD")
         raise e
 
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).decode('utf8')
     shutil.copyfile("tesseract", tmp_dir + '/tesseract')
+    shutil.copyfile("test.png", tmp_dir + '/test.png')
     os.chmod(tmp_dir + "/tesseract", 0755)
-    print("ver: " + tesserocr.tesseract_version())
+    os.chmod(tmp_dir, 0755)
+    os.chmod('/tmp', 0755)
+    print("before image file from s3")
     image_file = download_file(bucket, key)
+    print("before image file to PIL")
+    print("before OCR")
     result_file = tesseract(image_file)
-    contents = open(result_file + '.txt').read()
-    print("contents " + contents)
+    print("Print files in firectory")
     for file in os.listdir('/tmp'):
         print(file)
+    try:
+        print("before PyTessBaseAPI set 2")
+        api = PyTessBaseAPI(path=os.path.join(SCRIPT_DIR, 'tessdata'), lang='eng',psm=PSM.AUTO_OSD)
+        print("After API set")
+        api.SetImageFile(image_file)
+        print("After API set image")
+        print("TEXT from tesserocr: %s" % api.GetUTF8Text())
+        print("CONFIDENCE from tesserocr: %s" % api.AllWordConfidences())
+    except Exception:
+        pass
